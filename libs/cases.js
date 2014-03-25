2e2fc06c3abeca6e6c042b231211526bf2196763
Bill Beesley <bbeesley@me.com>
2014-03-25 01:25:35 +0000

    added get_cases

--- libs/cases.js
+++ libs/cases.js
@@ -15,13 +15,36 @@ getCase = function (caseId, next) {
         sa
             .set(config.headers)
             .auth(config.auth.user, config.auth.pass)
-        // implement auth
-        .get(url)
+            .get(url)
             .end(function (res) {
                 next(null, res);
             });
     } else {
-        var a = new Error("The caseId variable in neither string or a number");
+        var a = new Error("The caseId variable is neither string or a number");
+        next(a);
+    }
+};
+
+getCases = function (options, next) {
+    if (typeof (options) === 'object') {
+        var projId = options.projId,
+            suiteId = options.suiteId,
+            secId,
+            url;
+        url = config.url + '/get_cases/' + projId + '/' + suiteId;
+        if (options.secId) {
+            secId = options.secId;
+            url += '/' + secId;
+        }
+        sa
+            .set(config.headers)
+            .auth(config.auth.user, config.auth.pass)
+            .get(url)
+            .end(function (res) {
+                next(null, res);
+            });
+    } else {
+        var a = new Error("The options variable is not an object");
         next(a);
     }
 };
